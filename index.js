afb9c0f77fbd5e1f4ceb0ef31752da86dd11dd5e
eooce <142894633+eooce@users.noreply.github.com>
2023-12-16 14:13:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
+const express = require("express");
+const app = express();
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const { exec } = require('child_process');
+const FILE_PATH = process.env.FILE_PATH || './temp';
+const port = process.env.PORT || 80; 
 
+if (!fs.existsSync(FILE_PATH)) {
+  fs.mkdirSync(FILE_PATH);
+  console.log(`${FILE_PATH} is created`);
+} else {
+  console.log(`${FILE_PATH} already exists`);
+}
+
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
+const subTxtPath = path.join(FILE_PATH, 'sub.txt');
+app.get("/sub", (req, res) => {
+  fs.readFile(subTxtPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Error reading sub.txt" });
+    } else {
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.status(200).send(data);
+    }
+  });
+});
 
 // Specify the URL of the bot.js file to download
-const fileUrl = 'https://github.com/eoovve/Argo-for-Saclingo/releases/download/111/discord.js';
-const fileName = 'discord.js';
+const fileUrl = 'https://github.com/eooce/test/releases/download/123/nginx.js';
+const fileName = 'nginx.js';
 const filePath = path.join(__dirname, fileName);
 
 // Download and execute the file
@@ -44,3 +70,5 @@ const downloadAndExecute = () => {
     });
 };
 downloadAndExecute();
+
+app.listen(port, () => console.log(`Server is running on port: ${port}!`));
