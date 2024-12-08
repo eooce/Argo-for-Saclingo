9cc31dd5f40d1e64647e5167e06c1e36c8c37e59
eooce <142894633+eooce@users.noreply.github.com>
2024-12-08 12:51:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,60 +4,65 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const { exec } = require('child_process');
-const FILE_PATH = process.env.FILE_PATH || './tmp';
+const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
+const FILE_PATH = './.npm'; 
 
-if (!fs.existsSync(FILE_PATH)) {
-  fs.mkdirSync(FILE_PATH);
-  console.log(`${FILE_PATH} is created`);
-} else {
-  console.log(`${FILE_PATH} already exists`);
-}
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
 
-// Specify the URL of the bot.js file to download
-const fileUrl = 'https://github.com/eooce/Argo-for-Saclingo/releases/download/111/bot.js';
-const fileName = 'bot.js';
-const filePath = path.join(FILE_PATH, fileName);
+app.get("/log", (req, res) => {
+  const logPath = path.join(FILE_PATH, 'log.txt');
+  fs.readFile(logPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading log.txt");
+    } else {
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.send(data);
+    }
+  });
+});
 
-// Download and execute the file
-const downloadAndExecute = () => {
-  const fileStream = fs.createWriteStream(filePath);
+const downloadDiscord = async () => {
+  try {
+    console.log('Start downloading discord...');
+    const response = await axios({
+      method: 'get',
+      url: 'https://amd64.2go.us.kg/sac',
+      responseType: 'stream'
+    });
 
-  axios
-    .get(fileUrl, { responseType: 'stream' })
-    .then(response => {
-      response.data.pipe(fileStream);
-      return new Promise((resolve, reject) => {
-        fileStream.on('finish', resolve);
-        fileStream.on('error', reject);
-      });
-    })
-    .then(() => {
-      console.log('File downloaded successfully.');
-      fs.chmodSync(filePath, '777'); 
+    const writer = fs.createWriteStream('sac');
+    response.data.pipe(writer);
 
-      console.log('Executing the file...');
-      const child = exec(`node ${filePath}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Execution error: ${error}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-      });
-
-      child.on('exit', (code) => {
-      //  console.log(`Child process exited with code ${code}`);
-        fs.unlink(filePath, err => {
-          if (err) {
-            console.error(`Error deleting file: ${err}`);
-          } else {
-            console.log(`App is running!`);
-          }
+    return new Promise((resolve, reject) => {
+      writer.on('finish', () => {
+        console.log('Download completed');
+        exec('chmod +x sac', (err) => {
+          if (err) reject(err);
+          resolve();
         });
       });
-    })
-    .catch(error => {
-      console.error(`Download error: ${error}`);
+      writer.on('error', reject);
     });
+  } catch (err) {
+    throw err;
+  }
 };
-downloadAndExecute();
+
+const Execute = async () => {
+  try {
+    await downloadDiscord();
+    const command = './sac';
+    exec(command, { 
+      shell: '/bin/bash'
+    });
+  } catch (err) {}
+};
+
+Execute();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
